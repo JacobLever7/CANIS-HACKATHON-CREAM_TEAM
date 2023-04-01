d4ed69e646f329d68e4557fdc1c8341226ce8620
Jacob Lever <jacob.lever@ucalgary.ca>
2023-04-01 15:45:29 -0600

    make combined dataframe

--- code/main.py
+++ code/main.py
@@ -10,8 +10,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
+import ssl
+import certifi
+
+# Set the SSL certificate for NLTK
+os.environ['SSL_CERT_FILE'] = certifi.where()
+nltk.download('stopwords', download_dir='~/nltk_data')
+nltk.download('vader_lexicon', download_dir='~/nltk_data')
+
 from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.download('stopwords')
 nltk.download('vader_lexicon')
 
@@ -23,33 +31,39 @@ dir = os.path.dirname(path)
 dir = dir.replace('code', 'data')
 os.chdir(dir)
 
+
 df = pd.read_csv('archive/DataSet_Misinfo_FAKE.csv')
+df_real = pd.read_csv('archive/DataSet_Misinfo_TRUE.csv')
+
+#fix weird column name: 'Unnamed: 0'
+df.rename(columns= {'Unnamed: 0' : 'title'}, inplace=True)
+df_real.rename(columns= {'Unnamed: 0' : 'title'}, inplace=True)
 
 # Data Cleaning
 df.drop_duplicates(inplace=True)
 df.dropna(subset=['title', 'text'], inplace=True)
-df.drop(['uuid', 'ord_in_thread', 'published', 'language'], axis=1, inplace=True)
 
-# Exploratory Data Analysis
-topics = df['thread_title'].value_counts().nlargest(10)
-plt.figure(figsize=(10,5))
-sns.barplot(x=topics.values, y=topics.index, palette='Blues_r')
-plt.title('Most Common Topics of Misinformation and Fake News')
-plt.xlabel('Number of Articles')
-plt.ylabel('Topic')
-plt.show()
 
-sources = df['site_url'].value_counts().nlargest(10)
-plt.figure(figsize=(10,5))
-sns.barplot(x=sources.values, y=sources.index, palette='Greens_r')
-plt.title('Most Common Sources of Misinformation and Fake News')
-plt.xlabel('Number of Articles')
-plt.ylabel('Source')
-plt.show()
+# # Exploratory Data Analysis
+# topics = df['thread_title'].value_counts().nlargest(10)
+# plt.figure(figsize=(10,5))
+# sns.barplot(x=topics.values, y=topics.index, palette='Blues_r')
+# plt.title('Most Common Topics of Misinformation and Fake News')
+# plt.xlabel('Number of Articles')
+# plt.ylabel('Topic')
+# plt.show()
+
+# sources = df['site_url'].value_counts().nlargest(10)
+# plt.figure(figsize=(10,5))
+# sns.barplot(x=sources.values, y=sources.index, palette='Greens_r')
+# plt.title('Most Common Sources of Misinformation and Fake News')
+# plt.xlabel('Number of Articles')
+# plt.ylabel('Source')
+# plt.show()
 
 # Feature Engineering
-stop_words = set(stopwords.words('english'))
-vectorizer = TfidfVectorizer(stop_words=stop_words)
+#stop_words = set(stopwords.words('english'))
+vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(df['text'])
 df['title_sentiment'] = df['title'].apply(lambda x: SentimentIntensityAnalyzer().polarity_scores(x)['compound'])
 df['text_sentiment'] = df['text'].apply(lambda x: SentimentIntensityAnalyzer().polarity_scores(x)['compound'])
