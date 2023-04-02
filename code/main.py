ee81461ad47fad1cfb45dc77a5059cc1b615e8ba
JoriDuguid2 <joriduguid2@gmail.com>
2023-04-01 22:42:37 -0600

    start emotional analysis

--- code/main.py
+++ code/main.py
@@ -1,29 +1,12 @@
-# Import necessary libraries
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+from textblob import TextBlob
+
+# load the fake and real dataframes
+import pandas as pd
 import os
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-import nltk
-import ssl
-import certifi
-
-# Set the SSL certificate for NLTK
-os.environ['SSL_CERT_FILE'] = certifi.where()
-nltk.download('stopwords', download_dir='~/nltk_data')
-nltk.download('vader_lexicon', download_dir='~/nltk_data')
-
-from nltk.corpus import stopwords
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-nltk.download('stopwords')
-nltk.download('vader_lexicon')
-
-
+import matplotlib.pyplot as plt
+from textblob import TextBlob
 
 # Load the dataset
 path = os.path.realpath(__file__)
@@ -31,82 +14,45 @@ dir = os.path.dirname(path)
 dir = dir.replace('code', 'data')
 os.chdir(dir)
 
-
-df = pd.read_csv('archive/DataSet_Misinfo_FAKE.csv')
+df_fake = pd.read_csv('archive/DataSet_Misinfo_FAKE.csv')
 df_real = pd.read_csv('archive/DataSet_Misinfo_TRUE.csv')
 
-#fix weird column name: 'Unnamed: 0'
-df.rename(columns= {'Unnamed: 0' : 'title'}, inplace=True)
-df_real.rename(columns= {'Unnamed: 0' : 'title'}, inplace=True)
+def detect_sentiment(text):
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    return sentiment
 
-# Data Cleaning
-df.drop_duplicates(inplace=True)
-df.dropna(subset=['title', 'text'], inplace=True)
+# apply the detect_sentiment function to the 'text' column of each dataframe
+df_fake['sentiment_score'] = df_fake['text'].apply(detect_sentiment)
+df_real['sentiment_score'] = df_real['text'].apply(detect_sentiment)
 
+# assign a sentiment label based on the sentiment score
+df_fake.loc[df_fake['sentiment_score'] > 0, 'sentiment_label'] = 'positive'
+df_fake.loc[df_fake['sentiment_score'] == 0, 'sentiment_label'] = 'neutral'
+df_fake.loc[df_fake['sentiment_score'] < 0, 'sentiment_label'] = 'negative'
 
-# # Exploratory Data Analysis
-# topics = df['thread_title'].value_counts().nlargest(10)
-# plt.figure(figsize=(10,5))
-# sns.barplot(x=topics.values, y=topics.index, palette='Blues_r')
-# plt.title('Most Common Topics of Misinformation and Fake News')
-# plt.xlabel('Number of Articles')
-# plt.ylabel('Topic')
-# plt.show()
+df_real.loc[df_real['sentiment_score'] > 0, 'sentiment_label'] = 'positive'
+df_real.loc[df_real['sentiment_score'] == 0, 'sentiment_label'] = 'neutral'
+df_real.loc[df_real['sentiment_score'] < 0, 'sentiment_label'] = 'negative'
 
-# sources = df['site_url'].value_counts().nlargest(10)
-# plt.figure(figsize=(10,5))
-# sns.barplot(x=sources.values, y=sources.index, palette='Greens_r')
-# plt.title('Most Common Sources of Misinformation and Fake News')
-# plt.xlabel('Number of Articles')
-# plt.ylabel('Source')
-# plt.show()
+# create a list of sentiment categories
+sentiments = ['positive', 'neutral', 'negative']
 
-# Feature Engineering
-#stop_words = set(stopwords.words('english'))
-vectorizer = TfidfVectorizer(stop_words='english')
-X = vectorizer.fit_transform(df['text'])
-df['title_sentiment'] = df['title'].apply(lambda x: SentimentIntensityAnalyzer().polarity_scores(x)['compound'])
-df['text_sentiment'] = df['text'].apply(lambda x: SentimentIntensityAnalyzer().polarity_scores(x)['compound'])
-df['num_words'] = df['text'].apply(lambda x: len(x.split()))
+# create a dictionary to hold the frequency of each sentiment category for each dataframe
+freq_dict = {'Fake': [], 'Real': []}
+for sent in sentiments:
+    freq_dict['Fake'].append(df_fake['sentiment_label'].value_counts()[sent])
+    freq_dict['Real'].append(df_real['sentiment_label'].value_counts()[sent])
 
-# Machine Learning Modeling
-X_train, X_test, y_train, y_test = train_test_split(X, df['type'], test_size=0.3, random_state=42)
-
-# Logistic Regression
-lr = LogisticRegression()
-lr.fit(X_train, y_train)
-lr_preds = lr.predict(X_test)
-print('Logistic Regression Results:')
-print('Accuracy:', accuracy_score(y_test, lr_preds))
-print('Precision:', precision_score(y_test, lr_preds))
-print('Recall:', recall_score(y_test, lr_preds))
-print('F1 Score:', f1_score(y_test, lr_preds))
-print('Confusion Matrix:')
-print(confusion_matrix(y_test, lr_preds))
-
-# Random Forest
-rf = RandomForestClassifier()
-rf.fit(X_train, y_train)
-rf_preds = rf.predict(X_test)
-print('\nRandom Forest Results:')
-print('Accuracy:', accuracy_score(y_test, rf_preds))
-print('Precision:', precision_score(y_test, rf_preds))
-print('Recall:', recall_score(y_test, rf_preds))
-print('F1 Score:', f1_score(y_test, rf_preds))
-print('Confusion Matrix:')
-print(confusion_matrix(y_test, rf_preds))
-
-# Grid Search Cross Validation
-param_grid = {'n_estimators': [50, 100, 150],
-              'max_depth': [10, 20, 30, None]}
-rf_gs = GridSearchCV(RandomForestClassifier(), param_grid, cv=5)
-rf_gs.fit(X_train, y_train)
-rf_gs_preds = rf_gs.predict(X_test)
-print('\nRandom Forest with Grid Search Results:')
-print('Best Parameters:', rf_gs.best_params_)
-print('Accuracy:', accuracy_score(y_test, rf_gs_preds))
-print('Precision:', precision_score(y_test, rf_gs_preds))
-print('Recall:', recall_score(y_test, rf_gs_preds))
-print('F1 Score:', f1_score(y_test, rf_gs_preds))
-print('Confusion Matrix:')
-print(confusion_matrix(y_test, rf_gs_preds))
+# create a bar chart of the sentiment frequency for each dataframe
+bar_width = 0.35
+r1 = range(len(sentiments))
+r2 = [x + bar_width for x in r1]
+plt.bar(r1, freq_dict['Fake'], color='red', width=bar_width, label='Fake')
+plt.bar(r2, freq_dict['Real'], color='green', width=bar_width, label='Real')
+plt.xticks([r + bar_width/2 for r in r1], sentiments)
+plt.xlabel('Sentiment')
+plt.ylabel('Frequency')
+plt.title('Sentiment Frequency in Fake and Real News')
+plt.legend()
+plt.show()
